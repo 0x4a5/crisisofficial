c3d038fa4ad6f440b0f8d10c065b99b42cdc0177
{GenesisBlock/} <96637227+0x4a5@users.noreply.github.com>
2022-05-07 17:07:58 -0700

    gh-pages issues

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import ServicesPage from './routes/ServicesPage';
 import InvestorsPage from './routes/InvestorsPage';
 import AboutPage from './routes/AboutPage';
 import ContactPage from './routes/ContactPage';
-import ErrorPage from './routes/ErrorPage';
 
 
 
@@ -18,12 +17,12 @@ ReactDOM.render(
   <BrowserRouter basename={window.location.pathname || ''}>
     <Routes>
         {/* Crisis Routing Block */}
-       <Route path='*' element={<ErrorPage />} />
+      <Route exact path='/' element={<App />} />
       <Route path='/services' element={<ServicesPage />} />
       <Route path='/investors' element={<InvestorsPage />} />
       <Route path='/about' element={<AboutPage />} />
       <Route path='/contact' element={<ContactPage />} />
-      <Route exact path='/' element={<App />} />
+      
 
 
 
